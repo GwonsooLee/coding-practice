172d55811a3b88ba21aeeaceea643cac1bb2aeaf
Gwonsoo-Lee <59159138+GwonsooLee@users.noreply.github.com>
2025-04-27 00:06:41 +0900

    Title: 서로 다른 BST 개수 세기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250427/서로 다른 BST 개수 세기/number-of-unique-bst.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_N 20
+#define MOD 1000000007
+
+long long dp[MAX_N+1];
+
+long long func(int N) {
+   if (N == 0) return 1;
+
+   if (dp[N] != -1) return dp[N];
+
+   int ans = 0;
+   for (int i = 1; i <= N; ++i) {
+      ans += func(i-1) * func(N-i);
+   }
+   return dp[N] = ans;
+}
+
+int main()
+{
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+
+   int N;
+   cin >> N;
+
+   memset(dp, -1, sizeof(dp));
+   
+   cout << func(N) << "\n";
+
+   return 0;
+}
+
