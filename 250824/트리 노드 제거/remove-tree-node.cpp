5b4b0fe0ae26e0fcd7d29c3ba1c2ac3336179ff2
Gwonsoo-Lee <59159138+GwonsooLee@users.noreply.github.com>
2025-08-24 15:32:00 +0900

    Title: 트리 노드 제거, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250824/트리 노드 제거/remove-tree-node.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_N 300000
+int N; 
+
+unordered_map<int,vector<int>> G;
+
+int dfs(int node, int removed) {
+
+   if (node == removed) return 0;
+
+   if (G[node].size() == 0) return 1;
+
+   int ans = 0;
+   for (auto & x:G[node]) {
+      ans += dfs(x, removed);
+   }
+   return ans;
+}
+
+int main()
+{
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+
+   cin >> N;
+   vector<int>parent(N);
+   int rootNode = -1;
+   for (int i = 0; i < N; ++i) {
+      cin >> parent[i];
+      G[parent[i]].push_back(i);
+      if (parent[i] == -1) {
+         rootNode = i;
+      }
+   }
+   int x;
+   cin >> x;
+   cout << dfs(rootNode, x) << "\n";
+
+   return 0;
+}
+
