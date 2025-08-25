3faf701c76e1b0c452adfa9097795394ba1ba675
Gwonsoo-Lee <59159138+GwonsooLee@users.noreply.github.com>
2025-08-26 00:11:52 +0900

    Title: 트리 경유, Time: 55ms, Memory: 2MB, Status: Passed - Codetree

--- 250826/트리 경유/a-way-through-the-tree.cpp
+++ 250826/트리 경유/a-way-through-the-tree.cpp
@@ -4,20 +4,6 @@ using namespace std;
 int N, Q;
 bool vis[(1<<20)+1];
 
-int findLCA(int x, int y) {
-   unordered_set<int> tmp;
-   while(x > 0) {
-      tmp.insert(x);
-      x /= 2;
-   }
-
-   while(y > 0) {
-      if (tmp.find(y) != tmp.end()) return y;
-      y /= 2;
-   }
-   return -1;
-}
-
 
 int main()
 {
@@ -26,41 +12,21 @@ int main()
    cin >> N >> Q;
 
    memset(vis, false, sizeof(vis));
-   int curr = 1;
    for (int i = 0; i < Q; ++i) {
       int target;
       cin >> target;
-
-      int lca = findLCA(curr, target);
-      // cout << curr << "->" << target << "==>" << lca << "\n";
-      int now = curr;
-      while(now / 2 >= lca && vis[now/2] == false){
-         now /= 2;
-      }
-      if (now != lca) {
-         curr = now;
-         cout << curr << "\n";
-         continue;
-      }
-
-      now = target;
-      bool found = vis[target];
-      if (found) curr = target;
-      while(now / 2 >= lca) {
-         now /= 2;
+      int ans = 0;
+      int now = target;
+      while(now > 0) {
          if (vis[now]) {
-            curr = now;
-            found = true;
+            ans = now;
          }
+         now /= 2;
       }
+      cout << ans << "\n";
 
-      if (found) {
-         cout << curr << "\n";
-         continue;
-      } else {
+      if (ans == 0) {
          vis[target] = true;
-         curr = target;
-         cout << 0 << "\n";
       }
    }
 
