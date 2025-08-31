a8e8196520de0acdefbdcf4d2f552acd28f50943
Gwonsoo-Lee <59159138+GwonsooLee@users.noreply.github.com>
2025-08-31 16:01:24 +0900

    Title: 트리 위에 물건 놓기, Time: 75ms, Memory: 7MB, Status: Passed - Codetree

--- 250831/트리 위에 물건 놓기/node-best-count.cpp
+++ 250831/트리 위에 물건 놓기/node-best-count.cpp
@@ -13,25 +13,17 @@ void dfs(int node, int parent) {
    }
 
    int all_min = 0;
-   int all_excluded = 0;
-   int min_included = 1e9;
+   int all_included = 0;
    int y = -1;
    for (auto &x:G[node]) {
       if (x == parent) continue;
       dfs(x, node);
       all_min += min(dp[x][0], dp[x][1]);
-      all_excluded += dp[x][0];
-      if (dp[x][1] < min_included) {
-         min_included = dp[x][1];
-         y = x;
-      }
+      all_included += dp[x][1];
    }
 
    dp[node][1] = all_min+1;
-   dp[node][0] = all_min;
-   if (all_min == all_excluded) {
-      dp[node][0] = all_min + dp[y][1] - dp[y][0];
-   }
+   dp[node][0] = all_included;
 }
 
 
@@ -40,7 +32,6 @@ int main()
    ios::sync_with_stdio(false);
    cin.tie(nullptr);
 
-   memset(dp, 0x23, sizeof(dp));
    cin >> N;
 
    for (int i =0; i < N-1; ++i) {
@@ -51,7 +42,7 @@ int main()
 
    dfs(1,-1);
 
-   cout << min(dp[1][0], dp[1][0]) << "\n";
+   cout << min(dp[1][0], dp[1][1]) << "\n";
 
    return 0;
 }
